270845354f9294c9d799f5e566a8da97f092f317
MinaSameh1 <minasameh1@gmail.com>
2022-02-14 19:23:53 +0200

    Added Status Codes

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const log = require('../utils/logger')
 /* GET home page. */
 router.get('/', (_, res) => {
         log.error('Recieved NOTHINGG')
-        res.json('Nothing on this endpoint')
+        res.status(400).json('Nothing on this endpoint')
     })
 
 router.get('/healthcheck', (_, res) => {
         log.info('Recieved Health Check')
-        res.json('API Is up')
+        res.status(200).json('API Is up')
     })
 
 module.exports = router
