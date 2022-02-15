cccf87114a9ebb628f5d26fd63ce3a808728635f
MinaSameh1 <minasameh1@gmail.com>
2022-02-15 23:13:50 +0200

    feat: added better error handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,14 +5,15 @@ requird parm
 email,phone, password,first_name,last_name,gender,type,age
 */
 exports.postSignUp = (req, res) => {
-  createNewUser(req.body)
-    .then((newUser) => {
-      res.status(201).json(newUser)
-    })
-    .catch((err) => {
-      res.status(400).json({
-        error: true,
-        message: err
-      })
-    })
+  try {
+    const newUser = createNewUser(req.body)
+    if (newUser.error) return res.status(400).json(newUser.error)
+    return res.status(201).json(newUser.user)
+  } catch (err) {
+    res.status(500).json([
+      {
+        message: 'INTERNAL SERVER ERROR'
+      }
+    ])
+  }
 }
--- service/auth.service.js
+++ service/auth.service.js
@@ -10,9 +10,16 @@ exports.createNewUser = async (UserToBeAdded) => {
   const user = await User.exists({
     $or: [{ email: UserToBeAdded.email }, { phone: UserToBeAdded.phone }]
   })
-  if (user) throw ('email or phone number is already used')
+
+  if (user)
+    return {
+      error: 'email or phone number is already used',
+      user: null
+    }
   const newUser = await User.create({
     ...UserToBeAdded
   })
-  return omit(newUser, 'password')
-}
+	return { 
+		error: null,
+		user: omit(newUser, 'password')
+	}}
